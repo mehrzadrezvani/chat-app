7089d59a27112c9a617d32749df4fae166bcf927
mehrzadrezvani <mehrzadrezvani@gmail.com>
2024-12-03 23:32:40 +0330

    update on be

--- back-end/src/controllers/auth.controller.ts
+++ back-end/src/controllers/auth.controller.ts
@@ -1,11 +1,40 @@
+import { generateToken } from "../lib/utils";
+import User from "../models/user.model";
+import bcrypt from "bcryptjs";
+
 export const signup = (req, res) => {
   // res.send("signup route");
-  const { fullname, password, email } = req.body
+  const { fullName, password, email } = req.body;
   try {
-    
-  } catch (error) {
-    
-  }
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be atleast 6 characters!" });
+    }
+
+    const user = await User.findOne({ email });
+    if (user) return res.status(400).json({ message: "Email already exists!" });
+
+    const salt = await bcrypt.getSalt(10);
+    const hashedPass = await bcrypt.hashPass(password, salt);
+    const newUser = new User({
+      fullName,
+      email,
+      password: hashedPass,
+    });
+
+    if (!newUser) res.status(400).json({ message: "Invalid user data!" });
+    generateToken(newUser._id, res);
+    await newUser.save();
+    if (user)
+      return res
+        .status(201)
+        .json({
+          _id: newUser._id,
+          fullName: newUser.fullName,
+          profilePic: newUser.profilePic,
+        });
+  } catch (error) {}
 };
 
 export const login = (req, res) => {
