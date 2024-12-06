5a09de090ae876ce39e96611bebcf623426d3d2e
mehrzadrezvani <mehrzadrezvani@gmail.com>
2024-12-06 19:54:53 +0330

    update

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -2,9 +2,9 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./lib/db";
 import cookieParser from "cookie-parser";
-
 import AuthRoute from "./routes/auth.route";
 import MessageRoute from "./routes/message.route";
+import cors from "cors";
 
 dotenv.config();
 
@@ -12,6 +12,7 @@ const app = express();
 const PORT = process.env.PORT;
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({ origin: process.env.BASE_FE_URL, credentials: true }));
 app.use("/api/auth", AuthRoute);
 app.use("/api/messages", MessageRoute);
 
